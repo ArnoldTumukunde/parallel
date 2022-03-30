9c6aedeeefaebc5d61f80c70c0ead9b48b5b9841
Cheng JIANG <alex_cj96@foxmail.com>
2022-03-31 00:12:34 +0800

    fix
    Signed-off-by: Cheng JIANG <alex_cj96@foxmail.com>

--- scripts/helper/src/commands/mint.ts
+++ scripts/helper/src/commands/mint.ts
@@ -1,4 +1,4 @@
-import { getApi } from '@/utils'
+import { getApi } from '../utils'
 import { Command, CreateCommandParameters, program } from '@caporal/core'
 import { BN } from '@polkadot/util'
 import { decodeAddress, encodeAddress } from '@polkadot/util-crypto'
@@ -7,16 +7,15 @@ import os from 'os'
 import util from 'util'
 
 export default function ({ createCommand }: CreateCommandParameters): Command {
-  return createCommand('mint dot')
+  return createCommand('mint')
     .option('-p, --para-ws [url]', 'the parachain API endpoint', {
       default: 'wss://rpc.parallel.fi'
     })
     .option('-i, --input [csv]', 'the csv file which contains address,amount', {
       default: 'not_signed_users_cDot.csv'
     })
-    .option('-s, --ss58format [number]', "the address's ss58format", {
-      validator: program.NUMBER,
-      default: 0
+    .option('-o, --output [json]', 'the json file which contains all encoded calls', {
+      default: 'encoded-calls.json'
     })
     .option('-a, --asset-id [number]', 'the asset id to mint', {
       validator: program.NUMBER,
@@ -24,30 +23,31 @@ export default function ({ createCommand }: CreateCommandParameters): Command {
     })
     .action(async actionParameters => {
       const {
-        options: { paraWs, input, ss58format, assetId }
+        options: { paraWs, input, output, assetId }
       } = actionParameters
       const api = await getApi(paraWs.toString())
       const content = await util.promisify(fs.readFile)(input.toString(), 'utf8')
       const records = content
         .split(os.EOL)
+        .filter(Boolean)
         .slice(1)
         .map(x => x.replace('\r', '').split(','))
+      console.log(records.length)
       const calls = [],
         step = 50
       for (let i = 0; i < records.length; i += step) {
         const encoded = api.tx.utility
           .batchAll(
             records.slice(i, i + step).map(([address, amount]) => {
-              const subAddress = encodeAddress(
-                decodeAddress(address, true, ss58format.valueOf() as number),
-                42
-              )
-              return api.tx.assets.mint(assetId.valueOf() as number, subAddress, new BN(amount))
+              const subAddress = encodeAddress(decodeAddress(address, true, 0), 42)
+              const beneficiary = { Id: subAddress }
+              return api.tx.assets.mint(assetId.valueOf() as number, beneficiary, new BN(amount))
             })
           )
           .toHex()
-        calls.push(`0x${encoded.slice(6)}`)
+        calls.push(encoded)
       }
-      console.log(JSON.stringify(calls, null, 4))
+      await util.promisify(fs.writeFile)(output.toString(), JSON.stringify(calls, null, 4), 'utf8')
+      process.exit(0)
     })
 }
