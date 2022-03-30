9e959aeaac6353e5539d6543534b5402aca2d0a3
Cheng JIANG <alex_cj96@foxmail.com>
2022-03-31 01:02:30 +0800

    refactor
    Signed-off-by: Cheng JIANG <alex_cj96@foxmail.com>

--- scripts/helper/src/commands/mint.ts
+++ scripts/helper/src/commands/mint.ts
@@ -5,48 +5,60 @@ import { decodeAddress, encodeAddress } from '@polkadot/util-crypto'
 import * as fs from 'fs'
 import os from 'os'
 import util from 'util'
+import Keyring from '@polkadot/keyring'
+
+const readFile = util.promisify(fs.readFile)
+const writeFile = util.promisify(fs.writeFile)
+const BATCH_SIZE = 50
 
 export default function ({ createCommand }: CreateCommandParameters): Command {
   return createCommand('mint')
     .option('-p, --para-ws [url]', 'the parachain API endpoint', {
-      default: 'wss://rpc.parallel.fi'
+      default: 'wss://cj-dev-rpc.parallel.fi'
     })
     .option('-i, --input [csv]', 'the csv file which contains address,amount', {
-      default: 'not_signed_users_cDot.csv'
+      default: 'input.csv'
     })
     .option('-o, --output [json]', 'the json file which contains all encoded calls', {
-      default: 'encoded-calls.json'
+      default: 'output.json'
     })
     .option('-a, --asset-id [number]', 'the asset id to mint', {
       validator: program.NUMBER,
-      default: 101
+      default: 100
     })
     .action(async actionParameters => {
       const {
         options: { paraWs, input, output, assetId }
       } = actionParameters
       const api = await getApi(paraWs.toString())
-      const content = await util.promisify(fs.readFile)(input.toString(), 'utf8')
-      const records = content
+      const keyring = new Keyring({ type: 'sr25519' })
+      const signer = keyring.addFromUri(`${process.env.PARA_CHAIN_SUDO_KEY || '//Dave'}`)
+      const inputContent = await readFile(input.toString(), 'utf8')
+      const lines = inputContent
         .split(os.EOL)
         .filter(Boolean)
         .slice(1)
         .map(x => x.replace('\r', '').split(','))
-      console.log(records.length)
-      const calls = [],
-        step = 50
-      for (let i = 0; i < records.length; i += step) {
-        const encoded = api.tx.utility
-          .batchAll(
-            records.slice(i, i + step).map(([address, amount]) => {
-              const subAddress = encodeAddress(decodeAddress(address, true, 0), 42)
-              return api.tx.assets.mint(assetId.valueOf() as number, subAddress, new BN(amount))
-            })
-          )
-          .toHex()
-        calls.push(encoded)
+      const nonce = await api.rpc.system.accountNextIndex(signer.address)
+      const batch = []
+      for (let i = 0; i < lines.length; i += BATCH_SIZE) {
+        const chunk = lines.slice(i, i + BATCH_SIZE)
+        const calls = chunk.map(([address, amount]) => {
+          const subAddress = encodeAddress(decodeAddress(address, false), 42)
+          return api.tx.assets.mint(assetId.valueOf() as number, subAddress, new BN(amount))
+        })
+        const tx = api.tx.utility.batchAll(calls)
+        batch.push([tx, tx.toHex(), nonce.addn(1)])
       }
-      await util.promisify(fs.writeFile)(output.toString(), JSON.stringify(calls, null, 4), 'utf8')
+      // for (const [tx, , nonce] of batch) {
+      //   await tx.signAndSend(signer, { nonce })
+      // }
+      const outputContent = JSON.stringify(
+        batch.map(([, encoded]) => encoded),
+        null,
+        4
+      )
+      await writeFile(output.toString(), outputContent, 'utf8')
       process.exit(0)
     })
 }
